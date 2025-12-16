146c15faf348dc56fc625182cbe58d503b36a17b
RxSachaDev <sacha.work38@gmail.com>
2025-12-16 12:56:44 -0500

    2eme composant de la blackjack _table fait

--- app/view/blackjack_table_view.py
+++ app/view/blackjack_table_view.py
@@ -16,19 +16,44 @@ def blackjack_table_view() -> ft.Container:
                     bgcolor=ft.Colors.with_opacity(0.4, ft.Colors.BLACK),
                     border_radius=ft.border_radius.all(16),
                     border=ft.border.all(2, ft.Colors.with_opacity(0.3, "#EAB308")),
-                    padding=ft.padding.all(3),
                     content=ft.Column([
                         ft.Row([
-                            ft.Text("CROUPIER", color="#FACC15")
+                            ft.Text("CROUPIER", color="#FACC15", size=12)
                         ], alignment=ft.MainAxisAlignment.CENTER),
                         ft.Row([
                             card_widget(),
                             card_widget()
                         ], alignment=ft.MainAxisAlignment.CENTER)
-                    ])
+                    ], alignment=ft.MainAxisAlignment.CENTER)
                 )
-            ], alignment=ft.MainAxisAlignment.CENTER),    
-        ])
+            ], alignment=ft.MainAxisAlignment.CENTER),
+            ft.Row([
+                ft.Container(
+                    width=235,
+                    height=96,
+                    border_radius=ft.border_radius.all(16),
+                    gradient=ft.LinearGradient(
+                        begin=ft.alignment.center_left,
+                        end=ft.alignment.center_right,
+                        colors=["#EAB308", "#FACC15", "#EAB308"],
+                    ),
+                    shadow=ft.BoxShadow(
+                        spread_radius=1,
+                        blur_radius=10,
+                        color=ft.Colors.with_opacity(0.3, "#000000"),
+                        offset=ft.Offset(4, 4),
+                    ),
+                    content= ft.Column([
+                        ft.Row([
+                            ft.Text("ACTION RECOMMANDÉE", color=ft.Colors.BLACK, size=12),
+                        ], alignment=ft.MainAxisAlignment.CENTER),
+                        ft.Row([
+                            ft.Text("STAND", color=ft.Colors.BLACK, weight=ft.FontWeight.BOLD, size=36),
+                        ], alignment=ft.MainAxisAlignment.CENTER)
+                    ], alignment=ft.MainAxisAlignment.CENTER, spacing=0)
+                )
+            ], alignment=ft.MainAxisAlignment.CENTER)    
+        ], spacing=40)
     )
     return table
 
