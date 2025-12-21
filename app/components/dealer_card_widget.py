26c0e914c3efd776cfc882df31dad23132b7ab86
RxSachaDev <sacha.work38@gmail.com>
2025-12-21 05:17:08 -0500

    ajout des cursor pointer

--- app/components/dealer_card_widget.py
+++ app/components/dealer_card_widget.py
@@ -1,21 +1,24 @@
 import flet as ft
 
 def card_dealer_widget() -> ft.Container:
-    return ft.Container(
-        width=64,
-        height=96,
-        border_radius=ft.border_radius.all(8),
-        border=ft.border.all(2, "#4B5563"),
-        gradient=ft.LinearGradient(
-            begin=ft.alignment.top_left,
-            end=ft.alignment.bottom_right,
-            colors=["#374151", "#1F2937"],
-        ),
-        alignment=ft.alignment.center,
-        content=ft.Image(
-            src="assets/add_card.png",
-            width=24,
-            height=24,
-            fit=ft.ImageFit.CONTAIN,
+    return ft.GestureDetector(
+        mouse_cursor=ft.MouseCursor.CLICK,
+        content=ft.Container(
+            width=64,
+            height=96,
+            border_radius=ft.border_radius.all(8),
+            border=ft.border.all(2, "#4B5563"),
+            gradient=ft.LinearGradient(
+                begin=ft.alignment.top_left,
+                end=ft.alignment.bottom_right,
+                colors=["#374151", "#1F2937"],
+            ),
+            alignment=ft.alignment.center,
+            content=ft.Image(
+                src="assets/add_card.png",
+                width=24,
+                height=24,
+                fit=ft.ImageFit.CONTAIN,
+            )
         )
     )
--- app/components/player_card_widget.py
+++ app/components/player_card_widget.py
@@ -1,22 +1,25 @@
 import flet as ft
 
 def player_card_widget() -> ft.Container:
-    return ft.Container(
-        width=89,
-        height=64,
-        border_radius=ft.border_radius.all(8),
-        border=ft.border.all(2, "#4B5563"),
-        gradient=ft.LinearGradient(
-            begin=ft.alignment.top_left,
-            end=ft.alignment.bottom_right,
-            colors=["#374151", "#1F2937"],
-        ),
-        alignment=ft.alignment.center,
-        content=ft.Image(
-                src="assets/add_card.png",
-                width=24,
-                height=24,
-                fit=ft.ImageFit.CONTAIN,
+    return ft.GestureDetector(
+        mouse_cursor=ft.MouseCursor.CLICK,
+        content=ft.Container(
+            width=89,
+            height=64,
+            border_radius=ft.border_radius.all(8),
+            border=ft.border.all(2, "#4B5563"),
+            gradient=ft.LinearGradient(
+                begin=ft.alignment.top_left,
+                end=ft.alignment.bottom_right,
+                colors=["#374151", "#1F2937"],
+            ),
+            alignment=ft.alignment.center,
+            content=ft.Image(
+                    src="assets/add_card.png",
+                    width=24,
+                    height=24,
+                    fit=ft.ImageFit.CONTAIN,
+            )
         )
     )
     
