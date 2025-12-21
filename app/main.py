92717bea7da62ef522bed8a7a0451e2b125e75fe
RxSachaDev <sacha.work38@gmail.com>
2025-12-21 05:37:39 -0500

    Changement de la configuration de la main window

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 from view.main_window import main_window
 import flet as ft
 
+def main(page: ft.Page):
+    page.title = "Blacjack Counter"
+    page.add(main_window())
+
 if __name__ == "__main__":
-    ft.app(target=main_window)
+    ft.app(target=main)
--- app/view/main_window.py
+++ app/view/main_window.py
@@ -2,29 +2,31 @@ import flet as ft
 from view.blackjack_table_view import blackjack_table_view
 from view.control_bar import control_bar
 
-def main_window(page: ft.Page):
-    page.title = "Blacjack Counter"
-    page.window_width = 2000
-    page.window_height = 1400
-    page.add(
-        ft.ShaderMask(
-            content=ft.Text(
-                "Blackjack Counter",
-                size=36,
-                weight=ft.FontWeight.BOLD,
-            ),
-            shader=ft.LinearGradient(
-                begin=ft.alignment.center_left,
-                end=ft.alignment.center_right,
-                colors=[
-                    "#FACC15",
-                    "#FDE047",
-                    "#EAB308",
-                ],
-            ),
-            blend_mode=ft.BlendMode.SRC_IN,
-        )
+def main_window() -> ft.Container:
+    return ft.Container(
+        width=2000,
+        height=1400,
+        content= ft.Row([
+            ft.Column([
+                ft.ShaderMask(
+                    content=ft.Text(
+                        "Blackjack Counter",
+                        size=36,
+                        weight=ft.FontWeight.BOLD,
+                    ),
+                    shader=ft.LinearGradient(
+                        begin=ft.alignment.center_left,
+                        end=ft.alignment.center_right,
+                        colors=[
+                            "#FACC15",
+                            "#FDE047",
+                            "#EAB308",
+                        ],
+                    ),
+                    blend_mode=ft.BlendMode.SRC_IN,
+                ),
+                blackjack_table_view(),
+                control_bar(),
+            ])
+        ])
     )
-    page.add(blackjack_table_view())
-    page.add(control_bar())
-    return main_window
